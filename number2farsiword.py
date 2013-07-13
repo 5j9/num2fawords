d614dc483906f6666c2238c8d40db9f9c8b05ff0
dalba <dalba.wiki@gmail.com>
2013-07-13 22:42:54 +0430

    zero bug + some parts where rewritten
    * Now returns صفر when number is zero (bug)
    * Lists were moved outside of functions for better performance.
    * Some if-expressions where converted to if-statements (for better understandability and also easier conversion to Lua. Old code is still available, only is commented out)

--- number2farsiword.py
+++ number2farsiword.py
@@ -1,77 +1,91 @@
+#biggest supported length: "922337203685477580732132132132212239"
+#a Lua rewrite of this code is available at: http://fa.wikipedia.org/wiki/پودمان:عدد_به_حروف
+
+yekan = [u'',
+         u'يک',
+         u'دو',
+         u'سه',
+         u'چهار',
+         u'پنج',
+         u'شش',
+         u'هفت',
+         u'هشت',
+         u'نه']
+
+dahgan = [u'',
+          u'',
+          u'بيست',
+          u'سي',
+          u'چهل',
+          u'پنجاه',
+          u'شصت',
+          u'هفتاد',
+          u'هشتاد',
+          u'نود']
+
+sadgan = [u'',
+          u'يکصد',
+          u'دويست',
+          u'سيصد',
+          u'چهارصد',
+          u'پانصد',
+          u'ششصد',
+          u'هفتصد',
+          u'هشتصد',
+          u'نهصد']
+
+exceptions = [u'ده',
+              u'يازده',
+              u'دوازده',
+              u'سيزده',
+              u'چهارده',
+              u'پانزده',
+              u'شانزده',
+              u'هفده',
+              u'هجده',
+              u'نوزده']
+scale = [u'',
+        u' هزار',
+        u' ميليون ',
+        u' ميليارد',
+        u' بيليون',
+        u' بيليارد',
+        u' تريليون',
+        u' ترليارد',
+        u' کوآدريليون',
+        u' کادريليارد',
+        u' کوينتيليون',
+        u' کوانتينيارد']
 
 def threedigit_words(threedigit):
-    yekan = [u'',
-             u'يک',
-             u'دو',
-             u'سه',
-             u'چهار',
-             u'پنج',
-             u'شش',
-             u'هفت',
-             u'هشت',
-             u'نه']
-    
-    dahgan = [u'',
-              u'',
-              u'بيست',
-              u'سي',
-              u'چهل',
-              u'پنجاه',
-              u'شصت',
-              u'هفتاد',
-              u'هشتاد',
-              u'نود']
-    
-    sadgan = [u'',
-              u'يکصد',
-              u'دويست',
-              u'سيصد',
-              u'چهارصد',
-              u'پانصد',
-              u'ششصد',
-              u'هفتصد',
-              u'هشتصد',
-              u'نهصد']
-
-    exceptions = [u'ده',
-                  u'يازده',
-                  u'دوازده',
-                  u'سيزده',
-                  u'چهارده',
-                  u'پانزده',
-                  u'شانزده',
-                  u'هفده',
-                  u'هجده',
-                  u'نوزده']
-
     words = ''
-        
-    words = sadgan[int(threedigit[0])] +\
-            (u' و ' if threedigit[0] != '0' and threedigit[1:]!='00'  else '')
+    if threedigit[0] != '0' and threedigit[1:]!='00':
+        words = sadgan[int(threedigit[0])] + u' و '
+    else:
+        words = sadgan[int(threedigit[0])]
     if threedigit[1] == '1':
         words += exceptions[int(threedigit[2])]
     else:
-        words += dahgan[int(threedigit[1])] +\
-                 (u' و ' if threedigit[2] != '0' and threedigit[1]!='0' else '')
+        if threedigit[2] != '0' and threedigit[1]!='0':
+            words += dahgan[int(threedigit[1])] + u' و '
+        else:
+            words += dahgan[int(threedigit[1])]
         words += yekan[int(threedigit[2])]
+
+#old method:            
+##    words = sadgan[int(threedigit[0])] +\
+##            (u' و ' if threedigit[0] != '0' and threedigit[1:]!='00'  else '')
+##    if threedigit[1] == '1':
+##        words += exceptions[int(threedigit[2])]
+##    else:
+##        words += dahgan[int(threedigit[1])] +\
+##                 (u' و ' if threedigit[2] != '0' and threedigit[1]!='0' else '')
+##        words += yekan[int(threedigit[2])]
     return words
 
 def strnum2words(strnumber):
     if int(strnumber) == 0:
-        return 'صفر'
-
-    scale = [u'',
-            u' هزار',
-            u' ميليون ',
-            u' ميليارد',
-            u' بيليون',
-            u' بيليارد',
-            u' تريليون',
-            u' ترليارد',
-            u' کوآدريليون',
-            u' کادريليارد'
-            u' کوينتيليون',
-             u' کوانتينيارد']
+        return u'صفر'
 
     if len(strnumber) > len(scale) * 3:
         raise ValueError, 'Out of range!'
