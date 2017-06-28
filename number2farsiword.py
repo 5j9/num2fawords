2d5469f6458ff5e263fdf8e2d76a349c2eed8298
5j9 <5j9@users.noreply.github.com>
2017-06-28 12:06:41 +0430

    Remove Python 2 support and make it python 3 compatible

--- number2farsiword.py
+++ number2farsiword.py
@@ -1,126 +1,131 @@
-#biggest supported length: "922337203685477580732132132132212239"
+"""Provide functions to convert a number (int) to Persian words."""
 #a Lua rewrite of this code is available at: http://fa.wikipedia.org/wiki/پودمان:عدد_به_حروف
 
-yekan = [u'',
-         u'يک',
-         u'دو',
-         u'سه',
-         u'چهار',
-         u'پنج',
-         u'شش',
-         u'هفت',
-         u'هشت',
-         u'نه']
+YEKAN = [
+    '',
+    'يک',
+    'دو',
+    'سه',
+    'چهار',
+    'پنج',
+    'شش',
+    'هفت',
+    'هشت',
+    'نه',
+]
 
-dahgan = [u'',
-          u'',
-          u'بيست',
-          u'سي',
-          u'چهل',
-          u'پنجاه',
-          u'شصت',
-          u'هفتاد',
-          u'هشتاد',
-          u'نود']
+DAHGAN = [
+    '',
+    '',
+    'بيست',
+    'سي',
+    'چهل',
+    'پنجاه',
+    'شصت',
+    'هفتاد',
+    'هشتاد',
+    'نود',
+]
 
-sadgan = [u'',
-          u'يکصد',
-          u'دويست',
-          u'سيصد',
-          u'چهارصد',
-          u'پانصد',
-          u'ششصد',
-          u'هفتصد',
-          u'هشتصد',
-          u'نهصد']
+SADGAN = [
+    '',
+    'يکصد',
+    'دويست',
+    'سيصد',
+    'چهارصد',
+    'پانصد',
+    'ششصد',
+    'هفتصد',
+    'هشتصد',
+    'نهصد',
+]
 
-exceptions = [u'ده',
-              u'يازده',
-              u'دوازده',
-              u'سيزده',
-              u'چهارده',
-              u'پانزده',
-              u'شانزده',
-              u'هفده',
-              u'هجده',
-              u'نوزده']
-scale = [u'',
-        u' هزار',
-        u' ميليون ',
-        u' ميليارد',
-        u' بيليون',
-        u' بيليارد',
-        u' تريليون',
-        u' ترليارد',
-        u' کوآدريليون',
-        u' کادريليارد',
-        u' کوينتيليون',
-        u' کوانتينيارد']
+DAH_TA_BIST = [
+    'ده',
+    'يازده',
+    'دوازده',
+    'سيزده',
+    'چهارده',
+    'پانزده',
+    'شانزده',
+    'هفده',
+    'هجده',
+    'نوزده',
+]
 
-def threedigit_words(threedigit):
-    words = ''
-    if threedigit[0] != '0' and threedigit[1:]!='00':
-        words = sadgan[int(threedigit[0])] + u' و '
+SCALE = [
+    '',
+    ' هزار',
+    ' ميليون ',
+    ' ميليارد',
+    ' بيليون',
+    ' بيليارد',
+    ' تريليون',
+    ' ترليارد',
+    ' کوآدريليون',
+    ' کادريليارد',
+    ' کوينتيليون',
+    ' کوانتينيارد',
+]
+
+
+def _three_digit_words(threedigit):
+    if threedigit[0] != '0' and threedigit[1:] != '00':
+        words = SADGAN[int(threedigit[0])] + ' و '
     else:
-        words = sadgan[int(threedigit[0])]
+        words = SADGAN[int(threedigit[0])]
     if threedigit[1] == '1':
-        words += exceptions[int(threedigit[2])]
+        words += DAH_TA_BIST[int(threedigit[2])]
     else:
-        if threedigit[2] != '0' and threedigit[1]!='0':
-            words += dahgan[int(threedigit[1])] + u' و '
+        if threedigit[2] != '0' and threedigit[1] != '0':
+            words += DAHGAN[int(threedigit[1])] + ' و '
         else:
-            words += dahgan[int(threedigit[1])]
-        words += yekan[int(threedigit[2])]
-
-#old method:            
-##    words = sadgan[int(threedigit[0])] +\
-##            (u' و ' if threedigit[0] != '0' and threedigit[1:]!='00'  else '')
-##    if threedigit[1] == '1':
-##        words += exceptions[int(threedigit[2])]
-##    else:
-##        words += dahgan[int(threedigit[1])] +\
-##                 (u' و ' if threedigit[2] != '0' and threedigit[1]!='0' else '')
-##        words += yekan[int(threedigit[2])]
+            words += DAHGAN[int(threedigit[1])]
+        words += YEKAN[int(threedigit[2])]
     return words
 
-def strnum2words(strnumber):
-    if int(strnumber) == 0:
-        return u'صفر'
 
-    if len(strnumber) > len(scale) * 3:
-        raise ValueError, 'Out of range!'
-    
+def cardinal(strnumber):
+    if int(strnumber) == 0:
+        return 'صفر'
+
+    if len(strnumber) > len(SCALE) * 3:
+        raise ValueError('out of range')
+
     length = len(strnumber)
-    
-    if length%3 !=0:
-        strnumber = '0'*(3-length%3) + strnumber
-        
-    groups = len(strnumber)/3
+
+    if length % 3 != 0:
+        strnumber = '0' * (3-length % 3) + strnumber
+
+    groups = len(strnumber) // 3
     words = ''
     group = groups
     while group > 0:
-        threedigit = strnumber[group*3-3:group*3]
-        word3 = threedigit_words(threedigit)
+        threedigit = strnumber[group * 3 - 3:group * 3]
+        word3 = _three_digit_words(threedigit)
         
         if word3 != '' and group != groups:
             if words == '':
-                words = word3 + scale[groups-group]
+                words = word3 + SCALE[groups - group]
             else:
-                words = word3 + scale[groups-group] + u' و ' + words
+                words = word3 + SCALE[groups - group] + ' و ' + words
         else:
             words = word3 + words
 
         group -= 1
     return words
 
-def ordinal(nubmer):
-    words = strnum2words(nubmer)
-    if words[-2:] == u'سه':
-        return words[:-2] + u'سوم'
-    else:
-        return words + u'م'
-                     
-while True:
-    number = raw_input('Enter your number plz:\n')
-    print strnum2words(number)
-    print ordinal(number)
+
+def ordinal(number):
+    """Return the ordinal form of the number converted to words."""
+    words = cardinal(number)
+    if words[-2:] == 'سه':
+        return words[:-2] + 'سوم'
+    return words + 'م'
+
+
+if __name__ == '__main__':
+    while True:
+        n = input('Enter your number plz:\n')
+        print(cardinal(n))
+        print(ordinal(n))
