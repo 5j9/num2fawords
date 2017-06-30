617ee3175950f7dc5c8db45e25e46dbd991476ea
5j9 <5j9@users.noreply.github.com>
2017-06-30 18:23:47 +0430

    version='0.3'

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.3.dev1',
+    version='0.3',
 
     description='A tool to convert numbers (int, float) into Persian words',
     long_description=long_description,
