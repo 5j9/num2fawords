e4517e06630ae8fc02e9a9bbd46eb54408bb7a31
5j9 <5j9@users.noreply.github.com>
2018-01-17 13:40:42 +0330

    setup.py: Cleanup and update classifiers

--- setup.py
+++ setup.py
@@ -1,83 +1,38 @@
 """A setuptools based setup module."""
 
-# Always prefer setuptools over distutils
 from setuptools import setup, find_packages
-# To use a consistent encoding
-from codecs import open
 from os import path
 
-here = path.abspath(path.dirname(__file__))
 
-# Get the long description from the README file
+here = path.abspath(path.dirname(__file__))
 with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
     name='num2fawords',
-
-    # Versions should comply with PEP440.  For a discussion on single-sourcing
-    # the version across setup.py and the project code, see
-    # https://packaging.python.org/en/latest/single_source_version.html
     version='1.1.dev1',
-
     description='A tool to convert numbers (int, float) into Persian words',
     long_description=long_description,
-
-    # The project's main homepage.
     url='https://github.com/5j9/num2fawords',
-
-    # Author details
     author='5j9',
     author_email='5j9@users.noreply.github.com',
-
-    # Choose your license
     license='GNU General Public License v3.0',
-
-    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
-        # How mature is this project? Common values are
-        #   3 - Alpha
-        #   4 - Beta
-        #   5 - Production/Stable
-        'Development Status :: 3 - Alpha',
-
-        'Environment :: Console',
-
-        # Indicate who your project is intended for
+        'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
         'Intended Audience :: End Users/Desktop',
-
-        # Pick your license as you wish (should match "license" above)
         'License :: OSI Approved :: '
         'GNU General Public License v3 or later (GPLv3+)',
-
         'Natural Language :: English',
         'Natural Language :: Persian',
-
-        # Specify the Python versions you support here. In particular, ensure
-        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
-
         'Topic :: Text Processing :: General',
-        'Topic :: Utilities',
     ],
-
-    # What does your project relate to?
     keywords='convert number words farsi persian',
-
-    # You can just specify the packages manually here if your project is
-    # simple. Or you can use find_packages().
-    packages=find_packages(),  # exclude=['contrib', 'docs', 'tests']
-
-    # Alternatively, if you want to distribute just a my_module.py, uncomment
-    # this:
-    #   py_modules=["my_module"],
-
-    # List run-time dependencies here.  These will be installed by pip when
-    # your project is installed. For an analysis of "install_requires" vs pip's
-    # requirements files see:
-    # https://packaging.python.org/en/latest/requirements.html
+    packages=find_packages(),
     install_requires=[
         'typing;python_version<"3.5"',
         'singledispatch;python_version<"3.4"',
